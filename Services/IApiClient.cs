a964ea9c20fb725f862ff94247dd1b2c5990638c
Rizqial5 <bagusriski1@gmail.com>
2024-12-07 15:47:10 +0700

    first

---
+++ Services/IApiClient.cs
@@ -0,0 +1,15 @@
+using MarkDownTaking.API.Controllers;
+using MarkDownTaking.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarkDownTakingFrontEnd.Services
+{
+    public interface IApiClient
+    {
+        Task<ActionResult<IEnumerable<MDData>>> GetAllAsync();
+        Task<ActionResult<MDData>> GetByIdAsync(int id);
+        Task<ActionResult> PostMdFileAsync( IFormFile fileUpload);
+        IActionResult GenerateMDFile(RequestContent inputText);
+    }
+    
+}
